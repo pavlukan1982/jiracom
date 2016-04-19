b65f2971c004915e480b2fc1411742bb2319fda4
pavluk <pavlukan1982@gmail.com>
2016-04-19 19:25:06 +0300

    Initial commit

---
+++ src/main/java/com/epam/jiracom/commands/QuitCommand.java
@@ -0,0 +1,10 @@
+package com.epam.jiracom.commands;
+
+import com.beust.jcommander.Parameters;
+
+/**
+ * Created by Andrei_Pauliukevich1 on 4/19/2016.
+ */
+@Parameters(commandNames = "quit", commandDescription = "Exit the program")
+public class QuitCommand {
+}
